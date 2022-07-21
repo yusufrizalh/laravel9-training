7eded91ddeae1ecb8d02d4b16a13369ad9903f08
Yusuf Rizal <yusufrizalh@gmail.com>
2022-07-21 11:54:06 +0700

    laravel9 - manage employees table

--- database/migrations/2022_07_20_092529_create_employees_table.php
+++ database/migrations/2022_07_20_092529_create_employees_table.php
@@ -9,10 +9,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id();               // bigint, primary key, auto_increment
-            $table->string('name');     // varchar
-            $table->text('address');    // text
-            $table->timestamps();       // created_at, updated_at
+            $table->id();                   // bigint, primary key, auto_increment
+            $table->string('name', 50);     // varchar(50)
+            $table->text('address');        // text
+            $table->boolean('status')->default(false);  // true or false
+            $table->timestamps();           // created_at, updated_at
         });
     }
 
