ba1f11f933515c24b1bbdc41646f1281c529f606
Yusuf Rizal <yusufrizalh@gmail.com>
2022-07-27 15:43:28 +0700

    message

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} | Inixindo</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     {{ $styles }}
 </head>
 
@@ -15,7 +15,7 @@
     <div class="pt-4 mt-5">
         {{ $slot }}
     </div>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
