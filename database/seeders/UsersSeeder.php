5df8d2b681bdfa6dd1c84adf2184cb88d7169ba7
Yusuf Rizal <yusufrizalh@gmail.com>
2022-07-27 23:05:11 +0700

    laravel9 - returning back collect on UsersSeeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -12,5 +12,31 @@ class UsersSeeder extends Seeder
     public function run()
     {
         User::factory()->count(10)->create();
+
+        // collect([
+        //     [
+        //         'name' => 'Yusuf Rizal',
+        //         'email' => 'rizal@inixindo.co.id',
+        //         'password' => bcrypt('password'),
+        //         'email_verified_at' => now(),
+        //         'remember_token' => Str::random(12),
+        //     ],
+        //     [
+        //         'name' => 'Meliana Renata',
+        //         'email' => 'meliana.renata@email',
+        //         'password' => bcrypt('password'),
+        //         'email_verified_at' => now(),
+        //         'remember_token' => Str::random(12),
+        //     ],
+        //     [
+        //         'name' => 'Jennifer Paula',
+        //         'email' => 'jennifer_paula@example',
+        //         'password' => bcrypt('password'),
+        //         'email_verified_at' => now(),
+        //         'remember_token' => Str::random(12),
+        //     ]
+        // ])->each(function ($user) {
+        //     User::create($user);
+        // });
     }
 }
