a161200b86fc84485247c30dab3bbf7607de2591
Yusuf Rizal <yusufrizalh@gmail.com>
2022-08-04 20:32:40 +0700

    laravel9 - login refactoring

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -17,17 +17,13 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
+        $attributes = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
 
-        $user = User::whereEmail($request->email)->first();
-        if ($user) {
-            if (Hash::check($request->password, $user->password)) {
-                Auth::login($user);
-                return redirect('/')->with('success', 'You are now logged in!');
-            }
+        if (Auth::attempt($attributes)) {
+            return redirect('/')->with('success', 'You are now logged in!');
         }
 
         throw ValidationException::withMessages([
